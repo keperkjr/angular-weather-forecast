2d8a4202f8b4e5769087781a1001d9062fd53e8c
keperkjr <keperkjr@yahoo.com>
2021-12-15 19:34:35 -0800

    Added GeocodeLookup & GeocodeParse models

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GeocodeLookup } from './models/geocodeLookup';
+import { GeocodeParse } from './models/geocodeParse';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-weather-forecast';
+export class AppComponent implements OnInit {
+    title = 'My Programming Notes - Angular Weather Forecast';
+    geocodeLookup!: GeocodeLookup.Result;
+    geocodeParse!: GeocodeParse.Result;
+    
+    constructor() { 
+    }
+
+    ngOnInit() {
+    }    
 }
