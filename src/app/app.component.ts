c5cbac9a84865e0e6c6ff07da5964b01f6261800
keperkjr <keperkjr@yahoo.com>
2021-12-17 20:32:51 -0800

    Updated the way the nearest location is found

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { firstValueFrom } from 'rxjs';
+import { firstValueFrom, Observable } from 'rxjs';
 import { PositionStack } from './models/positionstack';
 import { WeatherBit } from './models/weatherbit';
 import { PositionStackApiService, WeatherBitApiService } from './services/api.service';
@@ -68,13 +68,18 @@ export class AppComponent implements OnInit {
         return firstValueFrom(this.http.get("http://api.ipify.org/?format=json"));
     }    
 
-    sortLocations(latitude: number, longitude: number, locationResults: PositionStack.Result) {
-        locationResults.data.sort((a: any, b: any) => {
-            let distanceA = Utils.getDistance(latitude, longitude, a.latitude, a.longitude);
-            let distanceB = Utils.getDistance(latitude, longitude, b.latitude, b.longitude);                
-            return distanceA - distanceB;
-        })
-    }
+    getNearestLocation(latitude: number, longitude: number, locationResults: PositionStack.Result) {
+        let nearestDistance = Number.MAX_VALUE;
+        let nearestLocation!: PositionStack.Location;
+        for (let location of locationResults.data) {
+            let distance = Utils.getDistance(latitude, longitude, location.latitude, location.longitude);
+            if (distance < nearestDistance) {
+                nearestLocation = location;
+                nearestDistance = distance;
+            }
+        }
+        return nearestLocation;
+    }    
 
     async querySearch(searchQuery: string) {
         if (this.lastSearchData.searchQuery != searchQuery) {
@@ -83,13 +88,15 @@ export class AppComponent implements OnInit {
             });
             // Get the location that is the shortest distance from the user
             if (this.currentLocation != null) {
-                this.sortLocations(this.currentLocation.latitude, this.currentLocation.longitude, locationResults);
+                this.selectedLocation = this.getNearestLocation(this.currentLocation.latitude, this.currentLocation.longitude, locationResults);
             }
-            this.selectedLocation = locationResults.data[0];
         }
 
         let longitude = this.selectedLocation.longitude;
         let latitude = this.selectedLocation.latitude;
+        this.lastSearchData.longitude = longitude;
+        this.lastSearchData.latitude = latitude;
+        this.lastSearchData.searchQuery = searchQuery;
 
         let forecast = await this.getForecast(latitude, longitude);
         this.currentWeather = forecast.currentWeather;   
@@ -107,6 +114,9 @@ export class AppComponent implements OnInit {
 
         let forecast = await this.getForecast(latitude, longitude);
         this.currentWeather = forecast.currentWeather;  
+
+        this.lastSearchData.longitude = longitude;
+        this.lastSearchData.latitude = latitude;        
     }
     
     async getForecast(latitude: number, longitude: number) {
@@ -123,21 +133,22 @@ export class AppComponent implements OnInit {
 
     // TODO: Only do this if location api is available
     async getLocation(type: LocationSearch.Type, options: LocationSearch.Options) {
-        let geocode!: PositionStack.Result;
+        let observable!: Observable<PositionStack.Result>;
         switch(type) {
             case LocationSearch.Type.IP:
-                geocode = await firstValueFrom(this.positionStackApi.getReverseSearch(options.ipAddress || ''));
+                observable = this.positionStackApi.getReverseSearch(options.ipAddress || '');
                 break;
             case LocationSearch.Type.SearchQuery:
-                geocode = await firstValueFrom(this.positionStackApi.getForwardSearch(options.searchQuery || ''));              
+                observable = this.positionStackApi.getForwardSearch(options.searchQuery || '');              
                 break;
             case LocationSearch.Type.GPS:
-                geocode = await firstValueFrom(this.positionStackApi.getReverseSearch(options.latitude || 0, options.longitude || 0));
+                observable = this.positionStackApi.getReverseSearch(options.latitude || 0, options.longitude || 0);
                 break;
             default:
                 throw new Error(`Unknown search type: ${type}`);
                 break;
         }
+        let geocode = await firstValueFrom(observable);
         console.log(geocode);
         if (geocode.error != null) {
             throw new RuntimeError.LocationError(geocode.error.message, type);
@@ -152,7 +163,7 @@ export class AppComponent implements OnInit {
                 case LocationSearch.Type.IP:                    
                     break;
                 case LocationSearch.Type.SearchQuery:
-                    alert(`Unable to display forecast. Please enter another search term and try again!`);               
+                    alert(`Unable to display forecast. Location could not be determined from the entered search term. Please enter another search term and try again!`);               
                     break;
                 case LocationSearch.Type.GPS:
                     alert(`Unable to display forecast. Location could not be detected from your current position. Please try again!`);
@@ -175,18 +186,17 @@ export class AppComponent implements OnInit {
             switch(type) {
                 case LocationSearch.Type.SearchQuery:
                     await this.querySearch(eventData.searchQuery);
-                    this.lastSearchData.searchQuery = eventData.searchQuery;
                     break;
                 case LocationSearch.Type.GPS:
-                    await this.gpsSearch(eventData.latitude, eventData.longitude);                
+                    await this.gpsSearch(eventData.latitude, eventData.longitude);                                    
                     break;
                 default:
                     throw new Error(`Unknown search type: ${type}`);
                     break;
             }
-
-            this.lastSearchData.longitude = eventData.longitude;
-            this.lastSearchData.latitude = eventData.latitude;
+            if (this.selectedLocation != null) {
+                console.log('Selected Location:', this.selectedLocation);
+            }
         } catch (error) {
             this.displayError(error);
         }
