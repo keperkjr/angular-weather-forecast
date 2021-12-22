ceb98050780f6e9c28b9347f62e57fcb2326c91a
keperkjr <keperkjr@yahoo.com>
2021-12-22 15:25:55 -0800

    Added a progress scroll indicator to the carousel

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -12,11 +12,15 @@ export class CarouselComponent implements OnInit {
     @ViewChild('content')
     content!: ElementRef;
 
+    @ViewChild('progress')
+    progress!: ElementRef;
+
     @Input()
     data!: any[];
 
     percent: number = 0;
     scrollWidth: number = 150;
+    progressLeft: number = 0;
 
     constructor() { }
 
@@ -39,8 +43,17 @@ export class CarouselComponent implements OnInit {
     adjustScrollPosition(adjustment: number) {
         this.content.nativeElement.scrollLeft += adjustment;
 
-        let newPercent = 100 * ((this.content.nativeElement.scrollLeft + adjustment) / (this.content.nativeElement.scrollWidth - this.content.nativeElement.clientWidth));
-        this.percent = Math.max(0, Math.min(newPercent, 100));
+        let newPercent = ((this.content.nativeElement.scrollLeft + adjustment) / (this.content.nativeElement.scrollWidth - this.content.nativeElement.clientWidth));
+        this.percent = Math.max(0, Math.min(newPercent, 1)) * 100;
+
+        let left = newPercent * ((this.content.nativeElement.clientWidth - this.progress.nativeElement.clientWidth));
+        
+        console.log('left:', left, 'percent:', this.percent, 
+            'this.content.nativeElement.clientWidth:', this.content.nativeElement.clientWidth,
+            'this.progress.nativeElement.clientWidth:', this.progress.nativeElement.clientWidth);
+
+        left = Math.max(0, Math.min(left, this.content.nativeElement.clientWidth - this.progress.nativeElement.clientWidth));
+        this.progress.nativeElement.style.left = left + 'px';
     }
 
     // Horizontal touch scroll
