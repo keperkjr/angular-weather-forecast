ce7ca047a400dc094e93b8f370e29ea81919ab00
keperkjr <keperkjr@yahoo.com>
2021-12-21 21:04:42 -0800

    Added a progress bar on the bottom of the carousel to indicate how much scroll is remaining

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -15,6 +15,9 @@ export class CarouselComponent implements OnInit {
     @Input()
     data!: any[];
 
+    percent: number = 0;
+    scrollWidth: number = 150;
+
     constructor() { }
 
     ngOnInit(): void {
@@ -22,15 +25,32 @@ export class CarouselComponent implements OnInit {
 
     ngAfterViewInit(): void {
         // Total width
-        // console.log(this.content.nativeElement.scrollWidth);
+        console.log('content.nativeElement.scrollWidth', this.content.nativeElement.scrollWidth);
+        console.log('content.nativeElement.scrollLeft', this.content.nativeElement.scrollLeft);
+        console.log('content.nativeElement.offsetWidth', this.content.nativeElement.offsetWidth);
+
+        // this.initialPosition = this.content.nativeElement.offsetWidth;
+        // this.endPosition = this.content.nativeElement.scrollWidth;
+
+        this.scrollPercent();
     }
 
     scrollLeft() {
-        this.content.nativeElement.scrollLeft -= 150;
-      }
-    
-      scrollRight() {
-        this.content.nativeElement.scrollLeft += 150;
-      }    
+        this.content.nativeElement.scrollLeft -= this.scrollWidth;
+        this.scrollPercent();
+    }
 
+    scrollRight() {
+        this.content.nativeElement.scrollLeft += this.scrollWidth;
+        this.scrollPercent()
+    } 
+    
+    scrollPercent() {
+        setTimeout(() => {
+            console.log('content.nativeElement.scrollLeft', this.content.nativeElement.scrollLeft);
+
+            this.percent = 100 * (this.content.nativeElement.scrollLeft / (this.content.nativeElement.scrollWidth - this.content.nativeElement.clientWidth));
+            console.log(this.percent);
+        }, 500);
+    }
 }
